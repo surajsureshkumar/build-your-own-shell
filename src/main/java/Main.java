1a2464ea134ac09181b395420b8ccc0395539aa0
surajsureshkumar <surajsureshkumar.98@gmail.com>
2025-02-26 15:44:55 -0800

    REPL implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
-        System.out.println("Making your own command shell using java");
+//        System.out.println("Making your own command shell using java");
         System.out.print("$ ");
 
         Scanner scanner = new Scanner(System.in);
