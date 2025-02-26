363845e5ea503b834509562863ef251a63e75b86
surajsureshkumar <surajsureshkumar.98@gmail.com>
2025-02-26 15:44:19 -0800

    REPL implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
+        System.out.println("Making your own command shell using java");
         System.out.print("$ ");
 
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        System.out.println(input + ": command not found");
+        while(scanner.hasNext()){
+            String input = scanner.nextLine();
+            System.out.println(input + ": command not found");
+        }
     }
 }
