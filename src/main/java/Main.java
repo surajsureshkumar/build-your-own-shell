6aefb97714a05a6e7a4055b043a52b18c4cdfbca
surajsureshkumar <surajsureshkumar.98@gmail.com>
2025-02-26 15:52:52 -0800

    Fixed Code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,19 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
 //        System.out.println("Making your own command shell using java");
+        System.out.print("$ ");
 
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+        String input = scanner.nextLine(); // takes the first input
 
-        while(true){
-            System.out.println("$ " + input + ": command not found");
+        // so the below do while is performed till the input is empty
+        do{
+            System.out.println(input + ": command not found");
+            System.out.print("$ ");
+            input = scanner.nextLine();
         }
+        while(!input.matches(""));
+        scanner.close();
+
     }
 }
