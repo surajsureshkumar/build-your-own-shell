a5db67388c10474cd89571571e48bc81f5db935a
surajsureshkumar <surajsureshkumar.98@gmail.com>
2025-02-26 15:36:10 -0800

    Fixed spacing to match output

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,6 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        System.out.println(input+ ":command not found");
+        System.out.println(input + ": command not found");
     }
 }
