101a5f716c9c2ee57f861b230eb4d470faeb7b2e
surajsureshkumar <surajsureshkumar.98@gmail.com>
2025-02-26 15:49:35 -0800

    Fixed Code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,12 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
 //        System.out.println("Making your own command shell using java");
-        System.out.print("$ ");
 
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        
+
         while(true){
-            System.out.println(input + ": command not found");
+            System.out.println("$ " + input + ": command not found");
         }
     }
 }
