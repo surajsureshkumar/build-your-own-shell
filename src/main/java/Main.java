527694a6848b3ba41750bb70fce99f413c9ac7d6
surajsureshkumar <surajsureshkumar.98@gmail.com>
2025-02-26 16:06:01 -0800

    Added Exit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,24 +4,29 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
 //        System.out.println("Making your own command shell using java");
-        System.out.print("$ ");
+//        System.out.print("$ ");
+//
+//        Scanner scanner = new Scanner(System.in);
+//        String input = scanner.nextLine(); // takes the first input
+//
+//        // so the below do while is performed till the input is empty
+//        do{
+//            System.out.println(input + ": command not found");
+//            System.out.print("$ ");
+//            input = scanner.nextLine();
+//        }
+//        while(!input.matches("")); // regex matching
+//        scanner.close();
 
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine(); // takes the first input
+        while (true) {
+            System.out.println("$ ");
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine(); // takes the first input
 
-        // so the below do while is performed till the input is empty
-        do{
-            System.out.println(input + ": command not found");
-            System.out.print("$ ");
-            input = scanner.nextLine();
-            if (input.equals("exit 0")){
+            if (input.equals("exit 0")) {
                 break;
             }
             System.out.println(input + ": command not found");
-
         }
-        while(!input.matches("")); // regex matching
-        scanner.close();
-
     }
 }
